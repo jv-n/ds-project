6e75ac0cc197d565521f0dfdf98d369ea5d4f97c
Luiz Roberto <lrbf@cin.ufpe.br>
2025-07-02 16:17:48 -0300

    componente do selo atual + accordion detalhamento (+ nivel selo atual)

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,27 +1,41 @@
+// page.tsx
 "use client";
 import Image from "next/image";
 import Navbar from "@/components/navbar";
 import Rodape from "@/components/rodape";
 import Cardacao from "@/components/card-de-acao";
+import SelosPage from "@/components/selo-atual/SelosPage"; // Importe o componente principal da página de selos
 import { useState } from "react";
 
 export default function Home() {
-  const [ativo, setAtivo] = useState("acoes");
+  const [ativo, setAtivo] = useState("acoes"); // O estado inicial é "acoes"
 
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar ativo={ativo} setAtivo={setAtivo} />
 
-      <div className="flex-grow flex justify-center items-center">
-        <Cardacao
-          nomeacao="Projeto Esperança"
-          descricao="A ONG atua com comunidades vulneráveis promovendo educação e saúde."
-          ods1="mulheres"
-          ods2="combate a fome"
-          ods3="animais"
-          ods4="ODS"
-          nomedaong="Instituto Viver Bem"
-        />
+      <div className="flex-grow flex justify-center items-center p-4">
+        {ativo === "acoes" && (
+          <Cardacao
+            nomeacao="Projeto Esperança"
+            descricao="A ONG atua com comunidades vulneráveis promovendo educação e saúde."
+            ods1="mulheres"
+            ods2="combate a fome"
+            ods3="animais"
+            ods4="ODS"
+            nomedaong="Instituto Viver Bem"
+          />
+        )}
+
+        {ativo === "minhas doacoes" && (
+          <div className="text-center text-xl font-semibold">
+            <p>Conteúdo da página "Minhas Doações" em breve!</p>
+          </div>
+        )}
+
+        {ativo === "selos" && (
+          <SelosPage /> 
+        )}
       </div>
 
       <Rodape />
--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -6,3 +6,12 @@ export { logoamassada };
 
 import predio from "./predio.svg";
 export { predio };
+
+import selobronze from "./selobronze.svg";
+export { selobronze };
+
+import seloprata from "./seloprata.svg";
+export { seloprata };
+
+import seloouro from "./seloouro.svg";
+export { seloouro };
