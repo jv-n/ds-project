06fdabee920f5c1db453def64939d5d7fe11a2b0
jv-n <jv.nascimento4@outlook.com>
2025-07-05 10:18:55 -0300

    fix(home):Took out certificate component from page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 import Certificate from "./components/certificate";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Certificate empresa="Coca-Cola" level="Prata" id="01jj123214oj12132" data_emissao="04/07/2025"/>
+      Oi Turma!
     </div>
   );
 }
