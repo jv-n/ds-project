5487af93c82cd8213248dd50e8704f52d4eb476a
jv-n <jv.nascimento4@outlook.com>
2025-07-07 16:49:36 -0300

    chore: tested component in page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,8 +2,8 @@ import Certificate from "./components/certificate";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Oi Turma!
+    <div className="flex flex-col items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Certificate empresa="Centro de Informatica" level="Prata" id="01jj123214oj12132" data_emissao="04/07/2025"/>
     </div>
   );
 }
