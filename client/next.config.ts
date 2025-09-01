5d662bb3b8fd62e3ebb5af4dc1d3738f370afac0
Wesley Silveira <wslc@cin.ufpe.br>
2025-08-31 23:25:01 -0300

    deploy

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,12 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  basePath: '/seloresponsa',
+  assetPrefix: '/seloresponsa',
+  trailingSlash: true,
   async rewrites() {
     return [
       {
         
         source: "/api/:path*",
-        destination: "http://localhost:3001/:path*", 
+        destination: "http://vm-cinboraimpactar2.cin.ufpe.br/seloresponsaback/:path*", 
       },
     ];
   },
